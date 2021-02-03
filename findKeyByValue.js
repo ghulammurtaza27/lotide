cbeb845e9058b5edcd72a18d00335c1b419ec4e0
Ghulam Murtaza <ghulam.murtaza@mail.mcgill.ca>
2021-02-03 12:22:26 +0000

    find val by key added

---
+++ findKeyByValue.js
@@ -0,0 +1,25 @@
+const assertEqual = function(actual, expected) {
+  if (actual !== expected) {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  } else {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  }
+};
+
+const findKeyByValue = function(input, valToCheck) {
+  for (let key in input) {
+    if (input[key] === valToCheck) {
+      return key;
+    }
+  }
+  return undefined;
+}
+
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
