e53f5cdbbb6190ad177076419073a1ab2924c12b
Ghulam Murtaza <ghulam.murtaza@mail.mcgill.ca>
2021-02-08 13:39:03 +0000

    Middle func and tail fun mocha and chai incorporated

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,8 +1,11 @@
-const eqArrays = require('../eqArrays');
-const assertArraysEqual = require("../assertArraysEqual");
 const middle = require("../middle");
 
+const assert = require('chai').assert;
+const tail = require('../middle');
 
 
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]) // => [2, 3]
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]) // => [3, 4]
+describe("#middle", () => {
+  it("returns [2, 3] for [1, 2, 3, 4]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4]), [2, 3]);
+  });
+});
