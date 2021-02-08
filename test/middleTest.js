065f9fb8aa2fcca7d294bad17b6b76bb31a17756
Ghulam Murtaza <ghulam.murtaza@mail.mcgill.ca>
2021-02-08 13:05:33 +0000

    Middle function implemented

---
+++ test/middleTest.js
@@ -0,0 +1,8 @@
+const eqArrays = require('../eqArrays');
+const assertArraysEqual = require("../assertArraysEqual");
+const middle = require("../middle");
+
+
+
+assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]) // => [2, 3]
+assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]) // => [3, 4]
